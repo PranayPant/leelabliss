8a80e552c8b566cf796c164824f27dc7c812a2ef
PranayPant <ppantg1@gmail.com>
2024-06-16 17:22:11 -0500

    feat: deploy to vercel

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createHashRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App.tsx";
 import "./index.css";
 
-const router = createHashRouter([
+const router = createBrowserRouter([
   {
-    path: "/",
+    path: "",
     element: <App />,
   },
   {
