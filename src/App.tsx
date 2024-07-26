e88bc4307938d493d77657e5ef820927b9917f1f
PranayPant <ppantg1@gmail.com>
2024-07-26 18:04:10 -0500

    style: landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Navbar } from "components/navbar";
+import { useIsAuthenticated } from "hooks/auth";
 import { StripeProvider } from "providers/auth/stripe";
 import { AuthenticationProvider } from "providers/auth/user";
 import { Suspense, lazy } from "react";
@@ -21,47 +22,50 @@ const LandingPage = lazy(
 );
 
 export function App() {
+  const isAuthenticated = useIsAuthenticated();
   return (
     <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <Suspense>
-              <LandingPage />
-            </Suspense>
-          }
-        />
-        <Route
-          path="/about"
-          element={
-            <Suspense>
-              <AboutPage />
-            </Suspense>
-          }
-        />
-        <Route element={<AuthenticationProvider />}>
-          <Route element={<StripeProvider />}>
+      <main className="app-container">
+        {isAuthenticated && <Navbar />}
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <Suspense>
+                <LandingPage />
+              </Suspense>
+            }
+          />
+          <Route
+            path="/about"
+            element={
+              <Suspense>
+                <AboutPage />
+              </Suspense>
+            }
+          />
+          <Route element={<AuthenticationProvider />}>
+            <Route element={<StripeProvider />}>
+              <Route
+                path="/home"
+                element={
+                  <Suspense>
+                    <HomePage />
+                  </Suspense>
+                }
+              />
+            </Route>
             <Route
-              path="/home"
+              path="/login"
               element={
                 <Suspense>
-                  <HomePage />
+                  <LoginPage />
                 </Suspense>
               }
             />
           </Route>
-          <Route
-            path="/login"
-            element={
-              <Suspense>
-                <LoginPage />
-              </Suspense>
-            }
-          />
-        </Route>
-      </Routes>
+        </Routes>
+      </main>
     </BrowserRouter>
   );
 }
--- src/pages/landing/landing.tsx
+++ src/pages/landing/landing.tsx
@@ -1,13 +1,16 @@
 import { LoginButton } from "components/login-button";
 import { useIsAuthenticated } from "hooks/auth";
 import { Navigate } from "react-router-dom";
+import styles from "./landing.module.css";
 
 export default function LandingPage() {
   const isAuthenticated = useIsAuthenticated();
   return (
-    <div>
-      <h1>Welcome to Leelabliss</h1>
-      {!isAuthenticated && <LoginButton />}
+    <div className={styles["container"]}>
+      <div>
+        <h1>Leelabliss</h1>
+        {!isAuthenticated && <LoginButton />}
+      </div>
       {isAuthenticated && <Navigate to="/home" />}
     </div>
   );
