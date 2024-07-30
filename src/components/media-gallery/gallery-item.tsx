009a3ac2c79ef42fb3823ca4eb654b7a8ccdf03d
PranayPant <ppantg1@gmail.com>
2024-07-30 18:44:49 -0500

    style: gallery item changes

--- src/components/media-gallery/gallery-item.tsx
+++ src/components/media-gallery/gallery-item.tsx
@@ -1,30 +1,9 @@
-import { useState } from "react";
-import { GalleryItemLoadingSkeleton } from "./loading-skeleton";
+import styles from "./gallery-item.module.css";
 
 export function GalleryItem({ url, alt }: { url: string; alt?: string }) {
-  const [isLoading, setIsLoading] = useState<boolean>(() => true);
-
-  const handleOnLoad = () => {
-    setIsLoading(false);
-  };
-
   return (
-    <>
-      {/* <div
-        style={{
-          display: isLoading ? "block" : "none",
-          width: "100%",
-          height: "100%",
-        }}
-      >
-        <GalleryItemLoadingSkeleton />
-      </div> */}
-      <img
-        // style={{ display: isLoading ? "none" : "block" }}
-        onLoad={handleOnLoad}
-        alt={alt}
-        src={url}
-      />
-    </>
+    <div className={styles["gallery-item"]}>
+      <img alt={alt} src={url} />
+    </div>
   );
 }
