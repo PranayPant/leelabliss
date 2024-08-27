168a330912765037e59bf914e39dd27f0ae09be9
PranayPant <ppantg1@gmail.com>
2024-08-27 13:33:26 -0500

    feat: use lambda function url

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -2,6 +2,6 @@ export const GET_S3_PRESIGNED_URL_ENDPOINT =
   "https://c4uddvotewsw7r5jrmtylqsdrq0nowss.lambda-url.us-east-1.on.aws";
 
 export const GET_S3_IMAGE_PATHS_ENDPOINT =
-  "https://ec0dmbzmo4.execute-api.us-east-1.amazonaws.com/dev/images";
+  "https://j4lnyt466bmeasbxjakgzrmqlq0axpxj.lambda-url.us-east-1.on.aws";
 
 export const IMGIX_SUBDOMAIN = "leelabliss-797188039.imgix.net";
