0e68c8365fd53bc5a4c236e46f5e7278e4d4d36d
PranayPant <ppantg1@gmail.com>
2024-08-26 22:00:03 -0500

    feat: use apigw endpoint

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -2,6 +2,6 @@ export const GET_S3_PRESIGNED_URL_ENDPOINT =
   "https://c4uddvotewsw7r5jrmtylqsdrq0nowss.lambda-url.us-east-1.on.aws";
 
 export const GET_S3_IMAGE_PATHS_ENDPOINT =
-  "https://c7g7wo4bddvfkrd2gpgjqgtujq0iuoao.lambda-url.us-east-1.on.aws";
+  "https://ec0dmbzmo4.execute-api.us-east-1.amazonaws.com/dev/images";
 
 export const IMGIX_SUBDOMAIN = "leelabliss-797188039.imgix.net";
